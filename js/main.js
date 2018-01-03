0468a4b5c1a4d749aaf5775f941a7cc5c0156050
Bryce McKenney <brycemckenney@gmail.com>
2018-01-03 15:20:50 -0800

    stripped unneeded code

--- js/main.js
+++ js/main.js
@@ -89,133 +89,15 @@ Math.log2 = Math.log2 || function (x) {
   return Math.log(x) / Math.LN2
 }
 
-// See : http://world.std.com/~reinhold/dicewarefaq.html#calculatingentropy
-function calcEntropyForWordOrSymbol (isSymbol) {
-  var entropy
-
-  if (!isSymbol) {
-    // ~ 12.9 bit of entropy per Diceware word.
-    entropy = new Big(Math.log2(7776))
-  } else {
-    // ~ 5.16 bits for special characters.
-    entropy = new Big(Math.log2(36))
-  }
-
-  return entropy
-}
-
-function calcCrackTime (numWords, guessesPerSec) {
-  var keySpace,
-    halfKeySpace,
-    seconds,
-    minutes,
-    hours,
-    days,
-    years,
-    millenia,
-    humanLifetimes,
-    universeLifetimes,
-    avgHumanLifespanInYears,
-    ageOfUniverseInYears
-
-  // https://en.wikipedia.org/wiki/Life_expectancy
-  avgHumanLifespanInYears = new Big(67.2)
-
-  // https://en.wikipedia.org/wiki/Age_of_the_universe
-  ageOfUniverseInYears = new Big(13798000000)
-
-  // https://xkcd.com/936/
-  // https://security.stackexchange.com/questions/62832/is-the-oft-cited-xkcd-scheme-no-longer-good-advice/62881#62881
-  // https://hashcat.net/forum/thread-2580.html
-  keySpace = new Big(Math.pow(7776, numWords))
-
-  // Divide the keySpace in half. On average it is expected that an
-  // exhaustive search of only half the keySpace will result in success.
-  halfKeySpace = keySpace.div(2)
-
-  // "Assume that your adversary is capable of a trillion guesses per second" - Snowden
-  // http://www.nytimes.com/2013/08/18/magazine/laura-poitras-snowden.html?pagewanted=all&_r=0
-  if (!guessesPerSec) {
-    guessesPerSec = new Big(1000000000000)
-  } else {
-    guessesPerSec = new Big(guessesPerSec)
-  }
-
-  seconds = halfKeySpace.div(guessesPerSec)
-  minutes = seconds.div(60)
-  hours = minutes.div(60)
-  days = hours.div(24)
-  years = days.div(365)
-  millenia = years.div(1000)
-  humanLifetimes = years.div(avgHumanLifespanInYears)
-  universeLifetimes = years.div(ageOfUniverseInYears)
-
-  // All values returned are of 'Big' type.
-  // See : https://mikemcl.github.io/big.js/
-  return {numWords: numWords, guessesPerSec: guessesPerSec, keySpace: keySpace, halfKeySpace: halfKeySpace, seconds: seconds, hours: hours, minutes: minutes, days: days, years: years, avgHumanLifespanInYears: avgHumanLifespanInYears, humanLifetimes: humanLifetimes, millenia: millenia, ageOfUniverseInYears: ageOfUniverseInYears, universeLifetimes: universeLifetimes}
-}
-
 // Lookup a word by its wordNum and return
 // an Array with a single word object suitable for displayWords.
 function getWordFromWordNum (wordNum) {
   if (wordNum.length === 5) {
     var word
-    switch (currentList) {
-      case 'alternative':
-        word = alternative[wordNum]
-        break
-      case 'basque':
-        word = basque[wordNum]
-        break
-      case 'catalan':
-        word = catalan[wordNum]
-        break
-      case 'danish':
-        word = danish[wordNum]
-        break
-      case 'diceware':
-        word = diceware[wordNum]
-        break
-      case 'dutch':
-        word = dutch[wordNum]
-        break
-      case 'esperanto':
-        word = esperanto[wordNum]
-        break
-      case 'finnish':
-        word = finnish[wordNum]
-        break
-      case 'french':
-        word = french[wordNum]
-        break
-      case 'german':
-        word = german[wordNum]
-        break
-      case 'japanese':
-        word = japanese[wordNum]
-        break
-      case 'maori':
-        word = maori[wordNum]
-        break
-      case 'norwegian':
-        word = norwegian[wordNum]
-        break
-      case 'polish':
-        word = polish[wordNum]
-        break
-      case 'swedish':
-        word = swedish[wordNum]
-        break
-      case 'spanish':
-        word = spanish[wordNum]
-        break
-      default:
-        word = eff[wordNum]
-        break
-    }
-    return [{'word': word, 'wordNum': wordNum, 'entropy': calcEntropyForWordOrSymbol(false)}]
+    word = eff[wordNum]
+    return [{'word': word, 'wordNum': wordNum}]
   } else if (wordNum.length === 2) {
-    return [{'word': special[wordNum], 'wordNum': wordNum, 'entropy': calcEntropyForWordOrSymbol(true)}]
+    return [{'word': special[wordNum], 'wordNum': wordNum}]
   }
 }
 
@@ -225,69 +107,17 @@ function displayWords (words) {
 
   // add the word to the global array of words
   $.each(words, function (index, obj) {
-    var objEntropy = new Big(obj.entropy)
-    totalEntropy = totalEntropy.plus(objEntropy)
-    $('#totalEntropy').text(totalEntropy.toFixed(2))
     wordList.push(obj.word)
   })
 
   // add the word to the main display
   $.each(words, function (index, obj) {
-    $('#diceWords').append('<li>' + obj.word + '<span class="text-muted">' + obj.wordNum + '</span></li>')
+    $('#diceWords').append('<li>' + obj.word + '</li>')
   })
-
-  $('#diceWordsCopyableSpace').text(wordList.join(' '))
-  $('#diceWordsCopyableDash').text(wordList.join('-'))
-  $('#diceWordsCopyableContainer').slideDown()
-}
-
-function displayCrackTime (words) {
-  $('#totalWords').text(words.length)
-
-  // Display crack time results
-  var crackTimeResults = calcCrackTime(words.length)
-
-  $('#crackTimeResultsGuessesPerSecond').text(addCommas(crackTimeResults.guessesPerSec.toFixed(0)))
-  $('#crackTimeResultsKeySpace').text(addCommas(crackTimeResults.keySpace.toFixed(0)))
-
-  $('#crackTimeResultsSeconds').text(
-    (crackTimeResults.seconds > 1) ? addCommas(crackTimeResults.seconds.toFixed(0)) : addCommas(crackTimeResults.seconds.toFixed(2))
-  )
-
-  $('#crackTimeResultsMinutes').text(
-    (crackTimeResults.minutes > 1) ? addCommas(crackTimeResults.minutes.toFixed(0)) : addCommas(crackTimeResults.minutes.toFixed(2))
-  )
-
-  $('#crackTimeResultsHours').text(
-    (crackTimeResults.hours > 1) ? addCommas(crackTimeResults.hours.toFixed(0)) : addCommas(crackTimeResults.hours.toFixed(2))
-  )
-
-  $('#crackTimeResultsDays').text(
-    (crackTimeResults.days > 1) ? addCommas(crackTimeResults.days.toFixed(0)) : addCommas(crackTimeResults.days.toFixed(2))
-  )
-
-  $('#crackTimeResultsYears').text(
-    (crackTimeResults.years > 1) ? addCommas(crackTimeResults.years.toFixed(0)) : addCommas(crackTimeResults.years.toFixed(4))
-  )
-
-  $('#crackTimeResultsHumanLifetimes').text(
-    (crackTimeResults.humanLifetimes > 1) ? addCommas(crackTimeResults.humanLifetimes.toFixed(0)) : addCommas(crackTimeResults.humanLifetimes.toFixed(6))
-  )
-
-  $('#crackTimeResultsMillenia').text(
-    (crackTimeResults.millenia > 1) ? addCommas(crackTimeResults.millenia.toFixed(0)) : addCommas(crackTimeResults.millenia.toFixed(7))
-  )
-
-  $('#crackTimeResultsUniverseLifetimes').text(
-    (crackTimeResults.universeLifetimes > 1) ? addCommas(crackTimeResults.universeLifetimes.toFixed(0)) : addCommas(crackTimeResults.universeLifetimes.toFixed())
-  )
-
-  $('#entropyEstimateContainer').slideDown()
 }
 
 function resetUI () {
   wordList = []
-  totalEntropy = new Big(0)
   $('#entropyEstimateContainer').hide()
   $('#listTitleHeader span').text(currentList)
   $('#diceWords').html('')
@@ -335,21 +165,5 @@ $(document).ready(function () {
       resetUI()
     }
     displayWords(getWords(numWords, numRolls))
-    displayCrackTime(wordList)
-  })
-
-  // add a word from the output from analog die rolls e.g. 14352
-  // can be either a five die roll for a full word, or a two die
-  // roll for a symbol.
-  // does not reset UI. Adds onto existing wordList
-  $('#addFiveDieRollWordForm').on('submit', function (e) {
-    var addFiveDieRollWord
-    e.preventDefault()
-    addFiveDieRollWord = $('#addFiveDieRollWord').val()
-    if ((addFiveDieRollWord.match(/^[1-6]{2}$/) || addFiveDieRollWord.match(/^[1-6]{5}$/)) && (addFiveDieRollWord.length === 2 || addFiveDieRollWord.length === 5)) {
-      displayWords(getWordFromWordNum(addFiveDieRollWord))
-    }
-    $('#addFiveDieRollWord').val('')
-    displayCrackTime(wordList)
   })
 })
